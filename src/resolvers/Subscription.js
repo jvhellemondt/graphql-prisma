380de81a2290f26b5e88c0b9c94442bde13110a8
Jens van Hellemondt <jens.vanhellemondt@gmail.com>
2020-07-16 12:41:21 +0200

    🔖 completed section 5

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,21 +1,20 @@
 const Subscription = {
-  comment: {
-    subscribe(parent, { postId }, { db, pubsub }, info) {
-      const post = db.posts.find(
-        ({ id, published }) => id === postId && published
-      )
+    comment: {
+        subscribe(parent, { postId }, { db, pubsub }, info){
+            const post = db.posts.find((post) => post.id === postId && post.published)
 
-      if (!post) {
-        throw new Error('Post not found!')
-      }
-      return pubsub.asyncIterator(`comment ${postId}`)
+            if (!post) {
+                throw new Error('Post not found')
+            }
+
+            return pubsub.asyncIterator(`comment ${postId}`)
+        }
     },
-  },
-  post: {
-    subscribe(parent, args, { pubsub }, info) {
-      return pubsub.asyncIterator('post')
-    },
-  },
+    post: {
+        subscribe(parent, args, { pubsub }, info) {
+            return pubsub.asyncIterator('post')
+        }
+    }
 }
 
 export { Subscription as default }
--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,29 +1,14 @@
 const User = {
-  credit(parent, args, { db }, info) {
-    const date = new Date()
-    let credit = parent.credit
-    switch (parent.calc) {
-      case 'INCL':
-        credit = credit * 1.21
-        break
-      case 'TAX':
-        credit = credit * 0.21
-        break
-      default:
-        break
+    posts(parent, args, { db }, info) {
+        return db.posts.filter((post) => {
+            return post.author === parent.id
+        })
+    },
+    comments(parent, args, { db }, info) {
+        return db.comments.filter((comment) => {
+            return comment.author === parent.id
+        })
     }
-    return credit
-  },
-  posts(parent, args, { db }, info) {
-    return db.posts.filter((post) => {
-      return post.author === parent.id
-    })
-  },
-  comments(parent, args, { db }, info) {
-    return db.comments.filter((comment) => {
-      return comment.author === parent.id
-    })
-  },
 }
 
 export { User as default }
